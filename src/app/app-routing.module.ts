54873c2cb5f50c23bbdf6569bb0ac73b2c749278
alexasfils <afilatov@pccube.com>
2025-02-16 12:45:46 +0100

    Create navbar componente, add new route creatre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ import { UserFormComponent } from './users/user-form/user-form.component';
 const routes: Routes = [
   { path: 'users', component: UsersListComponent },
   { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users/create', component: UserFormComponent, pathMatch: 'full' },
   { path: 'users/:id', component: UserFormComponent },
+  
 ];
 
 @NgModule({
